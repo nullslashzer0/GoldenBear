683093138df203592f997fa74f48a92aaef87c53
rwadsworth <robert.wadsworth@13ninjas.com>
2013-02-24 01:05:35 -0500

    moved new file functionality to menubar command; added theme selectino view view->editor menu

--- js/GoldenBear/GoldenBear.js
+++ js/GoldenBear/GoldenBear.js
@@ -1,43 +1,50 @@
 require([
-	"dojo/ready", "dojo/parser", 
-	"dojo/dom", "dojo/dom-style",
+	"dojo/ready", "dojo/parser", "dojo/on",
+	"dojo/dom", "dojo/dom-construct", "dojo/dom-style",
   "dijit/registry",
   "dijit/TitlePane",
   "dijit/MenuBar", "dijit/PopupMenuBarItem", "dijit/DropDownMenu",
-  "dijit/MenuItem",
+  "dijit/MenuItem", "dijit/PopupMenuItem",
 	"dijit/layout/BorderContainer", "dijit/layout/TabContainer", "dijit/layout/ContentPane"
 ], function(
-	ready, parser, 
-	dom, domStyle,
+	ready, parser, on,
+	dom, domConstruct, domStyle,
   registry,
   TitlePane,
   MenuBar, PopupMenuBarItem,DropDownMenu,
-  MenuItem,
+  MenuItem, PopupMenuItem,
 	BorderContainer, TabContainer, ContentPane
 ){
+  var codemirror = {
+    themes: [
+      'ambiance', 'blackboard','cobalt','eclipse','elegant','erlang-dark','lesser-dark',
+      'monokai','neat','night','rubyblue','solarized','twilight','vibrant-ink','xq-dark'
+    ]
+  };
+  
+  var openFiles = [];
+  
 	ready(function() {
-    var codemirror1 = CodeMirror(dom.byId('doc1'), {
-      mode: 'javascript',
-      lineNumbers: true,
-      value: 'function myScript(){ return 100; }\n'
-    });
-    
+    applicationMenu();
     registry.byId('documentEditor').watch('selectedChildWidget', function(name, oval, nval) {
       console.debug(name, " changed from ", oval, " to ", nval);
-      codemirror1.refresh();
+      
     });
     
-    applicationMenu();
     domStyle.set(dom.byId('app-splash'), 'display', 'none');
 	}); // End ready
   
+  /*********************************************************************
+   * APPLICATION MENU
+   ********************************************************************/
   function applicationMenu() {
     var menubar = new MenuBar({});
-    var filemenu = fileMenu();
+    var _file = fileMenu();
+    var _view = viewMenu();
     
     menubar.addChild(new PopupMenuBarItem({
       label: "File",
-      popup: filemenu
+      popup: _file
     }));
     
     menubar.addChild(new PopupMenuBarItem({
@@ -52,7 +59,7 @@ require([
      
     menubar.addChild(new PopupMenuBarItem({
       label: "View",
-      popup: new DropDownMenu({})
+      popup: _view
     }));
      
     menubar.addChild(new PopupMenuBarItem({
@@ -85,14 +92,71 @@ require([
     menubar.startup();
   }
   
+  /*********************************************************************
+   * File Menu
+   ********************************************************************/  
   function fileMenu() {
-    var dropDownMenu = new DropDownMenu({});
-    dropDownMenu.addChild(new MenuItem({
+    var menu = new DropDownMenu();
+    menu.addChild(new MenuItem({
       label: 'New',
       onClick: function() {
-        console.error('File->New command is not defined');
+        openFile();
       }
     }));
-    return dropDownMenu;
+    return menu;
+  }
+  
+  function openFile(name) {
+    var name = name || 'untitled';
+    var cmid = "document." + openFiles.length;  // codemirror id
+    var tc = registry.byId('documentEditor');
+    var cp = new ContentPane({
+      id: 'tab.' + cmid,
+      title: name,
+      closable: true
+    });
+    
+    var cm = CodeMirror(cp.containerNode, {
+      lineNumbers: true,
+    });
+    
+    openFiles.push(cm);
+    on(cp, 'show', function() { cm.refresh(); });
+    tc.addChild(cp);
+  }
+  
+  /*********************************************************************
+   * View Menu
+   ********************************************************************/  
+  function viewMenu() {
+    var menu = new DropDownMenu();
+    var editorMenu = new DropDownMenu();
+    var themesMenu = new DropDownMenu();
+    var themes = codemirror.themes.reverse();
+    var themesCount = themes.length;
+    while(--themesCount) {
+      themesMenu.addChild(new MenuItem({
+        label: codemirror.themes[themesCount],
+        onClick: function() {
+          var theme = this.label;
+          for(var i = 0, max = openFiles.length; i < max; i++) {
+            var cm = openFiles[i];
+            cm.setOption('theme', theme);
+          }
+        }
+      }));
+    }
+    
+    editorMenu.addChild(new PopupMenuItem({
+      label: 'Themes',
+      popup: themesMenu
+    }));
+    
+    menu.addChild(new PopupMenuItem({
+      label: 'Editor',
+      popup: editorMenu
+    }));
+      
+    return menu;
   }
 }); // end require
